d76d8d5363beca9904b8a0d4d76bee415389a7f6
aralagarsamy <aralagarsamy@egencia.com>
2019-10-10 14:35:10 -0700

    Oct10 - Day1

---
+++ basic/reversed_word.py
@@ -0,0 +1,18 @@
+def rev_word2(s):
+    words = []
+    length = len(s)
+    spaces = [' ']
+    
+    i = 0
+    
+    while i<length:
+        if s[i] not in spaces:
+            word_start = i
+            while i < length and s[i] not in spaces:
+                i+=1
+            words.append(s[word_start:i])
+        i+=1
+        
+    return " ".join(reversed(words))
+
+x = rev_word2('Hi John,   are you ready to go?')
