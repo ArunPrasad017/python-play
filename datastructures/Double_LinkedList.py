6ec3e9705d3ac1551f99db08168bf7f8bf03ba8c
aralagarsamy <aralagarsamy@egencia.com>
2020-02-15 14:58:42 -0800

    latest updates for DLL

---
+++ datastructures/Double_LinkedList.py
@@ -0,0 +1,38 @@
+class Node(object):
+    
+    def __init__(self,value, next = None, prev = None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+    
+    def insertToFirst(self,head,val_to_insert):
+        new_node = Node(val_to_insert)
+
+        new_node.next = head
+        new_node.prev = None
+
+        if head is not None:
+            self.head.prev = new_node
+        
+        self.head = new_node
+
+    def traverse_DLL(self,head):
+        currentNode = head
+        while currentNode is not None:
+            print(currentNode.value)
+            currentNode = currentNode.next
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+
+a.next = b
+a.prev = None
+b.next = c
+b.prev = a
+c.next = None
+c.prev = b
+
+a.traverse_DLL(a)
+a.insertToFirst(a,0)
+a.traverse_DLL(a)
--- datastructures/Singly_LinkedList.py
+++ datastructures/Singly_LinkedList.py
@@ -17,15 +17,37 @@ class Node(object):
         prevNode = None
         while currentNode is not None:
             if currentNode.value == val_to_delete:
+                # if value to be deleted is first node
                 if prevNode is None:
                     newhead = currentNode.next
                     currentNode.next = None
                     return newhead
                 prevNode.next = currentNode.next
                 return head
-                
             prevNode = currentNode
             currentNode=currentNode.next
+    
+    def insert_Node(self,head,val_to_Insert,position):
+        currentNode = head
+        prevNode = None
+        currentpos = 0
+        new_node = Node(val_to_Insert)
+        if not head:
+            head = new_node
+        if position==0:
+            currentNode.next = head
+            head = new_node
+            return head
+        while currentNode is not None:
+            if currentpos == position and currentNode.next:
+                prevNode.next = new_node
+                new_node.next = currentNode
+            elif currentNode.next == None:
+                currentNode.next = new_node
+                break
+            prevNode = currentNode
+            currentNode = currentNode.next
+            currentpos+=1
 
 a = Node(1)
 b = Node(2)
@@ -43,8 +65,11 @@ def print_LinkedList(head):
 
 head = a
 print(head)
-a.insert_Node_toEnd(a,5)
-print_LinkedList(a)
-a.delete_Node(a,2)
-print("after")
+# a.insert_Node_toEnd(a,5)
+# print_LinkedList(a)
+# a.delete_Node(a,2)
+# print("after")
+# print_LinkedList(a)
+
+a.insert_Node(a,5,3)
 print_LinkedList(a)
