303c7fb509657fd91d7ae73fb6077ea23cade82e
aralagarsamy <aralagarsamy@egencia.com>
2020-02-15 16:11:04 -0800

    latest updates for DLL

--- datastructures/Double_LinkedList.py
+++ datastructures/Double_LinkedList.py
@@ -7,7 +7,6 @@ class Node(object):
     
     def insertToFirst(self,head,val_to_insert):
         new_node = Node(val_to_insert)
-
         new_node.next = head
         new_node.prev = None
 
@@ -22,6 +21,41 @@ class Node(object):
             print(currentNode.value)
             currentNode = currentNode.next
 
+    def inserttoPosition(self,head,position,val_to_Insert):
+        if position == 0:
+            insertToFirst(head,val_to_Insert)
+        else:
+            new_node = Node(val_to_Insert)
+            currentNode = head
+            currentpos = 0
+            while position>currentpos and currentNode.next:
+                currentpos+=1
+                currentNode = currentNode.next
+            currentNode.prev.next = new_node
+            new_node.next = currentNode
+            new_node.prev = currentNode.prev
+            currentNode.prev = new_node
+        
+        return head
+    
+    def deleteNode(self,head,position):
+        if position == 0:
+            head = head.next
+            head.prev = None
+        else:
+            currentNode = head
+            currentpos = 0
+
+            while position>currentpos and currentNode.next:
+                currentpos+=1
+                currentNode = currentNode.next
+            currentNode.prev.next = currentNode.next
+            currentNode.prev = currentNode.next.prev
+            return head
+
+
+
+
 if __name__ == "__main__":
     a = Node(1)
     b = Node(2)
@@ -35,5 +69,12 @@ if __name__ == "__main__":
     c.prev = b
     
     a.traverse_DLL(a)
+    print("---------------------")
     new_head = a.insertToFirst(a,0)
+    a.traverse_DLL(new_head)
+    print("---------------------")
+    a.inserttoPosition(new_head,2,5)
+    a.traverse_DLL(new_head)
+    print("---------------------")
+    a.deleteNode(new_head,2)
     a.traverse_DLL(new_head)
