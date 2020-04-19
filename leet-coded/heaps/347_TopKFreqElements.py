68f1b8491fc68ae1a643cadf2f8d0ff4c537edbd
aralagarsamy <aralagarsamy@egencia.com>
2020-04-18 20:07:08 -0700

    heap_347

---
+++ leet-coded/heaps/347_TopKFreqElements.py
@@ -0,0 +1,12 @@
+import collections
+import heapq as hq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #egde case
+        if not nums:
+            return None
+        #creating a counter dictionary with elements frequency
+        count = collections.Counter(nums)
+        #using n largest heap function
+        # note - the heap offers multiple options to get keys and values and using two here for the same.
+        return hq.nlargest(k,count.keys(),key=count.get)
