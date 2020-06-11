3c38efc10bdd195c07d926a784f1f8895e97b0a5
aralagarsamy <aralagarsamy@egencia.com>
2020-06-10 22:20:44 -0700

    added leetcode problem

---
+++ coding-exercise/Day4-22-GenerateParentheses.py
@@ -0,0 +1,33 @@
+"""
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+if n =3
+
+Output:
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
+Solution: Uses the backtracking functionality
+"""
+import pytest
+def generateParenthesis(n):
+    res = []
+    backtrack(res, "", opened=0,closed=0,size=n)
+    return res
+
+def backtrack(res,string,opened,closed,size):
+    if len(string)==size*2:
+        res.append(string)
+    if opened<size:
+        backtrack(res, string+'(', opened+1,closed,size)
+    if closed<opened:
+        backtrack(res,string+')',opened,closed+1,size)
+
+n = 2
+print(generateParenthesis(n))
+
