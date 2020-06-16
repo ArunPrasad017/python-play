d15a36d895461a1ad9eaa95ad77acead9c55ce3c
aralagarsamy <aralagarsamy@egencia.com>
2020-06-15 23:30:53 -0700

    coding exercise adding backspacecomapre

---
+++ coding-exercise/LeetCode30daysProgram-June/April8th-BackSpaceStringCompare.py
@@ -0,0 +1,15 @@
+def backspaceCompare(S, T):
+    def stackCreator(string):
+        stack = []
+        for i,c in enumerate(string):
+            if c!='#':
+                stack.append(c)
+            elif stack:
+                stack.pop()
+        return stack
+    return stackCreator(S) == stackCreator(T)
+
+
+s1= "y#fo##f"
+s2= "y#f#o##f"
+print(backspaceCompare(s1,s2))
