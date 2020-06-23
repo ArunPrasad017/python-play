7f3d6101d4b7807b6bef1f90d5710d202fceaf17
aralagarsamy <aralagarsamy@egencia.com>
2020-06-22 23:11:39 -0700

    adding the Palindromic Permutation

---
+++ coding-exercise/Day16-266-PalindromePermutation.py
@@ -0,0 +1,24 @@
+    # Time limit Exceeded    
+    #     def isPalindrome(s):
+    #         return s == s[::-1]
+    #     lst = [''.join(p) for p in permutations(s)]
+    #     for item in lst:
+    #         if isPalindrome(item):
+    #             return True
+    #     return False
+
+def canPermutePalindrome(s):
+    dict_string = {}
+    for c in s:
+        if c not in dict_string:
+            dict_string[c]=1
+        else:
+            dict_string[c]+=1
+    count = 0
+    for k,v in dict_string.items():
+        count+=(v%2)
+    return count<=1
+
+#string = "aabbhijkkjih"
+string = "aab"
+print(canPermutePalindrome(string))
