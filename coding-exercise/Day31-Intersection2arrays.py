f1f35f1da6deba363283e0ecf9376ff29356bcff
aralagarsamy <aralagarsamy@egencia.com>
2020-07-08 19:33:10 -0700

    Day31

---
+++ coding-exercise/Day31-Intersection2arrays.py
@@ -0,0 +1,17 @@
+def intersect(nums1,nums2):
+    dict1 = {}
+    nums3 =[]
+    for c in nums1:
+        if c not in dict1:
+            dict1[c]=1
+        else:
+            dict1[c]+=1
+    for c in nums2:
+        if c in dict1 and dict1[c]!=0:
+            dict1[c]-=1
+            nums3.append(c)
+    return nums3
+
+nums1 = [4,9,5]
+nums2 = [9,4,9,8,4]
+print(intersect(nums1,nums2))
