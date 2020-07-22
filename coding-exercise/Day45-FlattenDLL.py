756bcd5d35800cc86c7de3fc5dda709b97fed099
aralagarsamy <aralagarsamy@egencia.com>
2020-07-22 01:27:40 -0700

    adding new file

---
+++ coding-exercise/Day45-FlattenDLL.py
@@ -0,0 +1,21 @@
+class Solution:
+    def flattenRec(self,prev,curr):
+        if not curr:
+            return prev
+        curr.prev = prev
+        prev.next = curr
+        
+        temp_next = curr.next
+        tail = self.flattenRec(curr, curr.child)
+        curr.child = None
+        return self.flattenRec(tail, temp_next)
+        
+        
+    def flatten(self, head: 'Node') -> 'Node':
+        if not head:
+            return head
+        #pseudo node technique
+        pseudoHead= Node(None, None, head, None)
+        self.flattenRec(pseudoHead,head)
+        pseudoHead.next.prev = None
+        return pseudoHead.next
