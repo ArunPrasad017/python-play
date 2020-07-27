b59fa952ad904f2d72312f3af4a3f7bc08ea5a0e
aralagarsamy <aralagarsamy@egencia.com>
2020-07-26 20:35:20 -0700

    added new files

---
+++ coding-exercise/Day50-AddDigits.py
@@ -0,0 +1,20 @@
+def AddDigits(num):
+    # looping and math
+    # if num==0:
+    #     return 0
+    # total = 0
+    # while num>0:
+    #     total+=num%10
+    #     num//=10
+    # return 9 if total%9==0 else total%9
+
+    # math only
+    if num==0:
+        return 0
+    if num%9==0:
+        return 9
+    else:
+        return num%9
+
+num=90071
+print(AddDigits(num))
