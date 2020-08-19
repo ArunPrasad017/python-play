187f03841c2f57b0c8c78516bc4246e026f257ae
aralagarsamy <aralagarsamy@egencia.com>
2020-08-18 23:12:42 -0700

    added for day 73

--- coding-exercise/Day73-PascalsTriangle.py
+++ coding-exercise/Day73-PascalsTriangle.py
@@ -1,3 +1,18 @@
+"""
+118. Pascal's Triangle
+
+Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+"""
 class Solution:
     def generate(self, N: int) -> List[List[int]]:
         res = []
