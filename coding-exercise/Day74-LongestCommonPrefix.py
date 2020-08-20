92db50aca94efe2a8e2c98b515319a387c0f8e10
aralagarsamy <aralagarsamy@egencia.com>
2020-08-19 22:39:20 -0700

    added new files-Day74

---
+++ coding-exercise/Day74-LongestCommonPrefix.py
@@ -0,0 +1,23 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: ["flower","flow","flight"]
+Output: "fl"
+"""
+
+def longestCommonPrefix(self, strs: List[str]) -> str:
+    if not(strs):
+        return ""
+    strs = sorted(strs)
+    prefix = strs[0]
+    for i in range(1,len(strs)):
+        temp_str = ""
+        for j in range(len(prefix)):
+            if prefix[j]==strs[i][j]:
+                temp_str+=prefix[j]
+            else:
+                break
+        prefix = temp_str
+    return prefix
