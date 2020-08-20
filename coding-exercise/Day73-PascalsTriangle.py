6c001ce7918d463094e831d1145db37d27e7c76c
Sourcery AI <>
2020-08-20 05:35:26 +0000

    'Refactored by Sourcery'

--- coding-exercise/Day73-PascalsTriangle.py
+++ coding-exercise/Day73-PascalsTriangle.py
@@ -18,16 +18,14 @@ class Solution:
         res = []
         if N==0:
             return res
-        level = 0
-        while level<N:
+        for level in range(N):
             if len(res)==level:
                 res.append([])
             level_length = len(res)
             i=level_length-1
             for j in range(level_length):
-                if j==0 or j==level_length-1:
+                if j in [0, level_length - 1]:
                     res[level].append(1)
                 else:
                     res[level].append(res[i-1][j-1] + res[i-1][j])
-            level+=1
         return res
