c666a6a557db6b8d8a211d5ff3159e50cdc519a5
aralagarsamy <aralagarsamy@egencia.com>
2020-08-28 23:08:37 -0700

    added 2 ,ore files

---
+++ coding-exercise/Day81-MoveZeros.py
@@ -0,0 +1,12 @@
+def moveZeroes(nums):
+    slowPtr, fastPtr = 0,0
+    for fastPtr in range(len(nums)):
+        if nums[fastPtr]!=0:
+            nums[fastPtr], nums[slowPtr] = nums[slowPtr], nums[fastPtr]
+        if nums[slowPtr]!=0:
+            slowPtr+=1
+        fastPtr+=1
+    return nums
+
+nums = [0,1,0,3,12]
+print(moveZeroes(nums))
