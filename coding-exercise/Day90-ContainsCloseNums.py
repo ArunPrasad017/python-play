c50bb47af2e3009c024fabd2fb8f4749f8178fc1
aralagarsamy <aralagarsamy@egencia.com>
2020-09-04 22:25:03 -0700

    added for Day90

---
+++ coding-exercise/Day90-ContainsCloseNums.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+from itertools import combinations
+def containsCloseNums(nums, k):
+    if not nums or k==0:
+        return False
+    if len(nums)==1:
+        return False
+    dict1 = defaultdict(list)
+    for i,n in enumerate(nums):
+        dict1[n].append(i)
+    for key,val in dict1.items():
+        if len(val)>1:
+            temp =[abs(a-b) for a,b in combinations(val,2)]
+            return any(i<=k for i in temp)
+    return False
