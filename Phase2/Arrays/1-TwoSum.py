a6cbfcd1523be8a1e8886cf61e741bc0d0e062e3
aralagarsamy <aralagarsamy@egencia.com>
2020-10-05 00:09:24 -0700

    added file

---
+++ Phase2/Arrays/1-TwoSum.py
@@ -0,0 +1,12 @@
+def twoSum(nums, target):
+    dict1,res ={},[]
+    for i,num in enumerate(nums):
+        dict1[num] = i
+    for i in range(len(nums)):
+        rem=target-nums[i]
+        if rem in dict1 and i!=dict1[rem]:
+            res.append(i)
+            res.append(dict1[rem])
+            return res
+
+print(twoSum([3,2,4],5))
