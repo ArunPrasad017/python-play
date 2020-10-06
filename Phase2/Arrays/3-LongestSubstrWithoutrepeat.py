a401880835d04842de5a1f51be4c096fb49a9f07
aralagarsamy <aralagarsamy@egencia.com>
2020-10-05 22:30:55 -0700

    updated file

--- Phase2/Arrays/3-LongestSubstrWithoutrepeat.py
+++ Phase2/Arrays/3-LongestSubstrWithoutrepeat.py
@@ -1,18 +1,20 @@
-def longest_without_repeat(string):
-    if not string:
+def longest_without_repeat(s):
+    if not s:
         return 0
-    res_lst =[string[0]]
-    max_len = 1
-    for i in range(1,len(string)):
-        if string[i] not in res_lst:
-            res_lst.append(string[i])
+    res_dict={}
+    l,output =0,0
+
+    for r in range(len(s)):
+        if s[r] not in res_dict:
+            output = max(output,r-l+1)
         else:
-            if len(res_lst)>max_len:
-                max_len=len(res_lst)
-                res_lst.pop(0)
-                res_lst.append(string[i])
-    return max(max_len,len(res_lst))
+            if res_dict[s[r]]<l:
+                output = max(output,r-l+1)
+            else:
+                l = res_dict[s[r]]+1
+        res_dict[s[r]] = r
+    return output
 
-
-s = 'pwwkew'
+s = 'abcdabde'
+#s = 'bbbbbb'
 print(longest_without_repeat(s))
