35c838370fc4fa4635452d31be9446841bee5bd1
aralagarsamy <aralagarsamy@egencia.com>
2020-11-10 21:48:33 -0800

    added for phase2-Day33

--- Phase2/educativeIO/GrockingCoding/CyclicSort/CyclicSort_FindMissing.py
+++ Phase2/educativeIO/GrockingCoding/CyclicSort/CyclicSort_FindMissing.py
@@ -1,7 +1,7 @@
 def find_missing_number(nums):
       # TODO: Write your code here
   i=0
-  while i<len(nums):
+  while i<len(nums)-1:
     j = nums[i]
     if nums[i]<len(nums) and nums[j]!=nums[i]:
       nums[i],nums[j] = nums[j],nums[i]
@@ -13,7 +13,7 @@ def find_missing_number(nums):
   return -1
 
 def main():
-    print(find_missing_number([1,4,2,0]))
+    print(find_missing_number([0,1,2,3,5]))
 
 if __name__ == "__main__":
     main()
