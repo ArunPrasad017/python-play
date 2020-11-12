52000bdfe6ef1bb6fe1ad7977b4f99070c9f80cd
Sourcery AI <>
2020-11-12 04:26:32 +0000

    'Refactored by Sourcery'

--- Phase2/educativeIO/GrockingCoding/CyclicSort/FindDuplicates.py
+++ Phase2/educativeIO/GrockingCoding/CyclicSort/FindDuplicates.py
@@ -1,5 +1,4 @@
 def find_all_duplicates(nums):
-    duplicateNumbers = []
     # TODO: Write your code here
     i,n=0,len(nums)
     while i<n:
@@ -8,7 +7,4 @@ def find_all_duplicates(nums):
             nums[i], nums[j] = nums[j], nums[i]    
         else:
             i+=1
-    for i in range(n):
-        if nums[i]!=i+1:
-            duplicateNumbers.append(nums[i])
-    return duplicateNumbers
+    return [nums[i] for i in range(n) if nums[i]!=i+1]
