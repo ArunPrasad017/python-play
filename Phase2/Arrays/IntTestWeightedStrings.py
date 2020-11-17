9b39b6b6f609cd269721bb62803212a498d6cb79
aralagarsamy <aralagarsamy@egencia.com>
2020-11-17 01:00:58 -0800

    updated for phase2

---
+++ Phase2/Arrays/IntTestWeightedStrings.py
@@ -0,0 +1,17 @@
+def weighted_strings(num):
+    d = {'a': 1, 'b': 3}
+    res = ""
+    for i in range(2,27):
+        key = chr(97+i)
+        prev_key = chr(96+i)
+        d[key] = ((i+1)*d[prev_key])+d[prev_key]
+    lst = list(d.keys())
+    for i in range(len(lst)-1,-1,-1):
+        r=num//d[lst[i]]
+        res+=lst[i]*r
+        num=num%d[lst[i]]
+        if num==0:
+            break
+    return res
+
+print(weighted_strings(25))
