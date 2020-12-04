ca57a651bea4f14fc0ae4f1b0d73d77deaa35054
aralagarsamy <aralagarsamy@egencia.com>
2020-12-04 00:30:50 -0800

    added for ph2

---
+++ Phase2/Arrays/CountingSort.py
@@ -0,0 +1,14 @@
+def sortColors(nums):
+    res=[]
+    temp=[0]*10
+    for i in range(len(nums)):
+        idx=nums[i]
+        temp[idx]+=1
+    for i in range(10):
+        while temp[i]>0:
+            res.append(i)
+            temp[i]-=1
+    return res
+
+nums=[2,0,2,1,1,0]
+print(sortColors(nums))
