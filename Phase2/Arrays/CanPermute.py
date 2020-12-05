52eef675c8837a3f929555f65129193ee07b6fd6
aralagarsamy <aralagarsamy@egencia.com>
2020-12-05 01:24:54 -0800

    added for ph2

---
+++ Phase2/Arrays/CanPermute.py
@@ -0,0 +1,13 @@
+from collections import Counter
+def canPermutePalindrome(s):
+    d=Counter()
+    for c in s:
+        d[c]+=1
+    cnt = sum((v%2) for k,v in d.items())
+    return cnt<=1
+
+
+s = "code"
+print(canPermutePalindrome(s))
+s = "racecar"
+print(canPermutePalindrome(s))
