5779d4606e63d3db0228372fa408516899292726
Sourcery AI <>
2021-01-27 08:43:15 +0000

    'Refactored by Sourcery'

--- p_2021/LeetCode/ArraysAndStrings/FB/ArrayProductExceptSelf.py
+++ p_2021/LeetCode/ArraysAndStrings/FB/ArrayProductExceptSelf.py
@@ -1,16 +1,10 @@
 def productExceptSelf(nums):
     l1 = [1]* len(nums)
     l2 = [1]* len(nums)
-    res = []
     for i in range(1,len(l1)):
         l1[i]=nums[i-1]*l1[i-1]
     for j in range(len(nums)-1,-1,-1):
-        if j==len(nums)-1:
-            l2[j]=1
-        else:
-            l2[j]= nums[j+1]*l2[j+1]
+        l2[j] = 1 if j==len(nums)-1 else nums[j+1]*l2[j+1]
     size = len(l2)
     i=0
-    for l,r in zip(l1,l2):
-        res.append(l*r)
-    return res
+    return [l*r for l,r in zip(l1,l2)]
