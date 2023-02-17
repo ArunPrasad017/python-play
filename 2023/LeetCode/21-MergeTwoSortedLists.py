3b1490389b48eed8216c43830abff257100656d9
Sourcery AI <>
2023-02-17 07:23:34 +0000

    'Refactored by Sourcery'

--- 2023/LeetCode/21-MergeTwoSortedLists.py
+++ 2023/LeetCode/21-MergeTwoSortedLists.py
@@ -4,19 +4,19 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(list1, list2):
+    def mergeTwoLists(self, list2):
         list3 = ListNode(0)
         curr = list3
-        while list1 and list2:
-            if list1.val <= list2.val:
-                curr.next = ListNode(list1.val)
-                list1 = list1.next
-            elif list1.val > list2.val:
+        while self and list2:
+            if self.val <= list2.val:
+                curr.next = ListNode(self.val)
+                self = self.next
+            else:
                 curr.next = ListNode(list2.val)
                 list2 = list2.next
             curr = curr.next
-        if list1:
-            curr.next = list1
+        if self:
+            curr.next = self
         elif list2:
             curr.next = list2
 
