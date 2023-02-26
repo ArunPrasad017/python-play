3cec39fd1a29e360683111deb09bb44b0abe27f2
Sourcery AI <>
2023-02-26 22:04:10 +0000

    'Refactored by Sourcery'

--- 2023/LeetCode/1523-CountOddNumbers.py
+++ 2023/LeetCode/1523-CountOddNumbers.py
@@ -2,8 +2,7 @@ def countOdds(low: int, high: int) -> int:
     res = 0
     if (low & 1) == 0:
         low += 1
-    res = (high - low) // 2 + 1
-    return res
+    return (high - low) // 2 + 1
 
 
 print(countOdds(3, 7))
